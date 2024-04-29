dff531d9c9e082ee3c0e9d7d5f8b757c9589aa4a
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-04-28 22:06:23 -0700

    problem naive_ddp_benchmarking

--- cs336-systems/expt/ddp.py
+++ cs336-systems/expt/ddp.py
@@ -55,6 +55,7 @@ def main(store, **kwargs):
         rank = int(os.environ["RANK"])
         local_rank = int(os.environ["LOCAL_RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
+        local_world_size = int(os.environ["LOCAL_WORLD_SIZE"])
 
         dist.init_process_group(
             backend=kwargs["backend"],
