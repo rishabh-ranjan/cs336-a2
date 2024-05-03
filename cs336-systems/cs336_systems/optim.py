59f12f23a6d6064a8dc75cb9012d9d895b9fe1e6
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-05-03 15:56:17 -0700

    problem optimizer_state_sharding

--- cs336-systems/cs336_systems/optim.py
+++ cs336-systems/cs336_systems/optim.py
@@ -7,45 +7,35 @@ import torch.distributed as dist
 
 class ShardedOptimizer(optim.Optimizer):
     def __init__(self, params, optimizer_cls, **kwargs):
-        # self.rank_wise_params = [[] for _ in range(dist.get_world_size())]
-        # self.rank_wise_param_size = torch.zeros(
-        #     dist.get_world_size(), dtype=torch.int64
-        # )
-        self.local_param_groups = []
-        params = list(params)
+        self.rank_wise_params = [[] for _ in range(dist.get_world_size())]
+        self.rank_wise_param_size = torch.zeros(
+            dist.get_world_size(), dtype=torch.int64
+        )
+        self.param_groups = []
         super().__init__(params, {})
-        # self.optimizer = optimizer_cls(self.local_param_groups, **kwargs)
-        self.optimizer = optimizer_cls(params, **kwargs)
+        self.optimizer = optimizer_cls(self.param_groups, **kwargs)
 
     def step(self, closure=None, **kwargs):
-        print(f"step")
         self.optimizer.step(closure, **kwargs)
 
-        # handles = []
-        # for rank, params in enumerate(self.rank_wise_params):
-        #     for param in params:
-        #         handle = dist.broadcast(param.detach(), rank, async_op=True)
-        #         handles.append(handle)
+        handles = []
+        for rank, params in enumerate(self.rank_wise_params):
+            for param in params:
+                handle = dist.broadcast(param.detach(), rank, async_op=True)
+                handles.append(handle)
 
-        # for handle in handles:
-        #     handle.wait()
+        for handle in handles:
+            handle.wait()
 
     def add_param_group(self, param_group):
-        self.local_param_groups.append(param_group)
-        pass
-        # print(f"param_group has {len(param_group['params'])} parameters")
-        # local_params = []
-        # for param in param_group["params"]:
-        #     param_rank = self.rank_wise_param_size.argmin()
-        #     param_size = param.numel() * param.element_size()
-        #     self.rank_wise_params[param_rank].append(param)
-        #     self.rank_wise_param_size[param_rank] += param_size
-        #     if param_rank == dist.get_rank():
-        #         local_params.append(param)
-        # print(f"local_params has {len(local_params)} parameters")
+        local_params = []
+        for param in param_group["params"]:
+            param_rank = self.rank_wise_param_size.argmin()
+            param_size = param.numel() * param.element_size()
+            self.rank_wise_params[param_rank].append(param)
+            self.rank_wise_param_size[param_rank] += param_size
+            if param_rank == dist.get_rank():
+                local_params.append(param)
 
-        # param_group["params"] = local_params
-        # self.local_param_groups.append(param_group)
-        # print(
-        #     f"self.local_param_groups[-1]['params'] has {len(self.local_param_groups[-1]['params'])} parameters"
-        # )
+        param_group["params"] = local_params
+        self.param_groups.append(param_group)
--- cs336-systems/tests/test_sharded_optimizer.py
+++ cs336-systems/tests/test_sharded_optimizer.py
@@ -18,10 +18,7 @@ from .common import (
 
 @pytest.mark.parametrize("model_class", [ToyModel, ToyModelWithTiedWeights])
 def test_sharded_optimizer(model_class):
-    # world_size = 2
-    # TODO: revert
-    world_size = 1
-    print(f"{world_size=}")
+    world_size = 2
     mp.spawn(
         _test_sharded_optimizer,
         args=(world_size, model_class),
@@ -34,14 +31,11 @@ def _test_sharded_optimizer(
     rank: int, world_size: int, model_class: Type[torch.nn.Module]
 ):
     # Use gloo backend for CPU
-    print(f"{rank=}, {world_size=}")
     device = _setup_process_group(rank=rank, world_size=world_size, backend="gloo")
-    print(f"{device=}")
     torch.manual_seed(42)
     optimizer_cls = torch.optim.AdamW
     # Since we've seeded, model states should be the same across ranks without having to broadcast.
     non_sharded_model = model_class().to(device)
-    print(f"{non_sharded_model=}")
 
     non_sharded_optimizer = optimizer_cls(
         non_sharded_model.parameters(),
@@ -50,9 +44,7 @@ def _test_sharded_optimizer(
         betas=(0.9, 0.999),
         eps=1e-8,
     )
-    print(f"{non_sharded_optimizer=}")
     sharded_model = deepcopy(non_sharded_model)
-    print(f"{sharded_model=}")
     sharded_optimizer = get_sharded_optimizer(
         sharded_model.parameters(),
         optimizer_cls,
@@ -61,20 +53,8 @@ def _test_sharded_optimizer(
         betas=(0.9, 0.999),
         eps=1e-8,
     )
-    print(f"{sharded_optimizer=}")
 
-    for step in range(10):
-        print(f"Rank {rank}, Step {step}")
-
-        # Check that the final model weights are the same regardless of if we're using
-        # the sharded or non-sharded optimizer.
-        for non_sharded_parameters, sharded_parameters in zip(
-            non_sharded_model.parameters(), sharded_model.parameters()
-        ):
-            numpy.testing.assert_allclose(
-                non_sharded_parameters.detach().cpu().numpy(),
-                sharded_parameters.detach().cpu().numpy(),
-            )
+    for _ in range(10):
         non_sharded_optimizer.zero_grad()
         sharded_optimizer.zero_grad()
 
@@ -100,17 +80,13 @@ def _test_sharded_optimizer(
         non_sharded_optimizer.step()
         sharded_optimizer.step()
 
-        # Check that the final model weights are the same regardless of if we're using
-        # the sharded or non-sharded optimizer.
-        for non_sharded_parameters, sharded_parameters in zip(
-            non_sharded_model.parameters(), sharded_model.parameters()
-        ):
-            numpy.testing.assert_allclose(
-                non_sharded_parameters.detach().cpu().numpy(),
-                sharded_parameters.detach().cpu().numpy(),
-            )
+    # Check that the final model weights are the same regardless of if we're using
+    # the sharded or non-sharded optimizer.
+    for non_sharded_parameters, sharded_parameters in zip(
+        non_sharded_model.parameters(), sharded_model.parameters()
+    ):
+        numpy.testing.assert_allclose(
+            non_sharded_parameters.detach().cpu().numpy(),
+            sharded_parameters.detach().cpu().numpy(),
+        )
     _cleanup_process_group()
-
-
-if __name__ == "__main__":
-    test_sharded_optimizer(ToyModel)
